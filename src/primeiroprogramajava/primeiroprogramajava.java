046dad8ebaa69ef16249275770ac7d496c3891d1
pfds2022 <palomafsantos25@gmail.com>
2022-07-26 23:00:30 -0300

    First Commit

---
+++ src/primeiroprogramajava/primeiroprogramajava.java
@@ -0,0 +1,26 @@
+package primeiroprogramajava;
+
+import br.com.dio.Gato;
+
+public class primeiroprogramajava {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+	/*	int a = 2;
+		int b = 3;
+    System.out.println("Hello wold " + (a+b)); */
+	}
+
+}
+
+
+class Livros {
+	private String nome;
+	private String paginas;
+	
+}
